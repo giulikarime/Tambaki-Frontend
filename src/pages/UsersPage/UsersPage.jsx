8e6b23c25a8a200e923ef98961f455a0eba6f827
giulikarime <giulianakarimedl@gmail.com>
2026-09-10 15:08:37 -0300

    feat: Adding modals and styling users page.

--- src/pages/UsersPage/UsersPage.jsx
+++ src/pages/UsersPage/UsersPage.jsx
@@ -3,16 +3,48 @@ import Header from "../../components/HeaderAndSidebar/Header";
 import Sidebar from "../../components/HeaderAndSidebar/Sidebar";
 import { useNavigate } from "react-router-dom";
 import { ChevronLeft, Funnel, Plus, Search } from "lucide-react";
-import { getUsers } from "../../services/user";
+import { getUsers, createUsers } from "../../services/user";
 import './users_page.css'
+import { AuthContext } from "../../services/AuthContext";
+import Modal from 'react-modal'
 
 function UsersPage(){
 
     const [expanded, setExpand] = useState(false);
     const [hasInteracted, setHasInteracted] = useState(false);
     const navigate = useNavigate();
+    const [formError,setFormError] = useState('');
 
     const [allUsers,setAllUsers] = useState([]);
+    const {user} = useState(AuthContext);
+
+    const [rolevalue,setRoleValue] = useState('');
+    const [accessLevelValue,setAccessLevelValue] = useState('');
+    const [employTypeValue,setEmployTypeValue] = useState('');
+    const [shiftValue,setShiftValue] = useState('');
+
+    const [usersEnums,setUsersEnums] = useState({
+        roles: [],
+        access_levels: [],
+        employ_types: [],
+        shifts: [],
+    })
+
+    const inputValues = [
+        {label: 'Nome Completo', model: 'input', type: 'text', name: 'name_add_user'},
+        {label: 'CPF', model: 'input', type: 'text', name: 'cpf_add_user'},
+        {label: 'Email', model: 'input', type: 'email', name: 'email_add_user'},
+        {label: 'Telefone', model: 'input', type: 'text', name: 'phone_add_user'},
+        {label: 'Senha', model: 'input', type: 'password', name: 'pass_add_user', readOnly: true},
+        {label: 'Cargo', model: 'select', name: 'role_add_user'},
+        {label: 'Nível de Acesso', model: 'select', name: 'access_level_add_user'},
+        {label: 'Modelo de Contrato', model: 'select', name: 'employ_add_user'},
+        {label: 'Horário', model: 'select', name: 'shift_add_user'},
+        {label: 'Data de Contratação', model: 'input', type: 'date', name: 'date_add_user'},
+        {label: 'Carga Horária', model: 'input', type: 'number', name: 'hours_add_user'},
+        {label: 'Salário', model: 'input', type: 'number', name: 'salary_add_user'},
+        {label: 'Banco', model: 'input', type: 'text', name: 'bank_add_user'},
+    ]
 
     async function handleGetUsers() {
         try{
@@ -22,6 +54,38 @@ function UsersPage(){
             console.error("Erro ao carregar usuários.",error);
         }
     };
+
+    async function handleCreateUser(e) {
+        e.preventDefalt();
+        setFormError('');
+        const formData = new FormData(e.target);
+        const payload = {
+            name: String(formData.get('')),
+            cpf: String(formData.get('')),
+            email: String(formData.get('')),
+            phone: String(formData.get('')),
+            password: String(formData.get('')),
+            role: rolevalue || usersEnums.roles[0],
+            access_level: accessLevelValue || usersEnums.access_levels[0],
+            employ_type: employTypeValue || usersEnums.employ_types[0],
+            shift: shiftValue || usersEnums.shifts[0],
+            hire_date: new Date(formData.get('')).toISOString(),
+            weekly_hours: String(formData.get('')),
+            salary: parseFloatString(formData.get('')),
+            bankName: String(formData.get('')),
+            active: String(formData.get('')),
+            storeUnitId: user.storeUnitId
+        }
+
+        try{
+            await createUsers(payload);
+            await handleGetUsers();
+            e.target.reset()
+        } catch(error){
+            console.log("Erro ao criar usuário.",error);
+            setFormError("Erro ao criar usuário. Verifique os dados e tente novamente.")
+        }
+    }
 
     useEffect(()=>{
         handleGetUsers();
@@ -36,7 +100,7 @@ function UsersPage(){
                     <div className="top-container-users">
                         <div style={{display:'flex',flexDirection:'row',alignItems:'center',gap:15}}>
                             <button className="btn-back-base" onClick={()=>navigate(-1)}><ChevronLeft></ChevronLeft></button>
-                            <h1>FUNCIONÁRIOS</h1>
+                            <h1>Funcionários</h1>
                         </div>
                         <div style={{display:'flex',flexDirection:'row',alignItems:'center',gap:15}}>
                             <button id='btn-plus-stock'><Plus></Plus></button>
@@ -69,29 +133,61 @@ function UsersPage(){
                     </div>
                     <div className="container-users">
                         <table>
-                            <thead>
+                            <thead className="thead-style tr-th-group">
                                 <th>NOME</th>
                                 <th>CARGO</th>
-                                <th>TELEFONE</th>
                                 <th>EMAIL</th>
+                                <th>TELEFONE</th>
                                 <th>NÍVEL DE ACESSO</th>
                                 <th>INFORMAÇÕES</th>
                             </thead>
                             <tbody>
                                 {allUsers.map((employees,index)=>(
-                                    <tr key={index}>
-                                        <td>{employees.name}</td>
-                                        <td>{employees.role}</td>
-                                        <td>{employees.email}</td>
-                                        <td>{employees.phone}</td>
-                                        <td>{employees.access_level}</td>
-                                        <td><button>Ver mais</button></td>
-                                    </tr>
+                                    <>
+                                        <tr className="tr-th-group" key={index}>
+                                            <td>{employees.name}</td>
+                                            <td>{employees.role}</td>
+                                            <td>{employees.email}</td>
+                                            <td>{employees.phone}</td>
+                                            <td>{employees.access_level}</td>
+                                            <td><button className="see-more-users">Ver mais</button></td>
+                                        </tr>
+                                    </>
                                 ))}
                             </tbody>
                         </table>
                     </div>
                 </div>
+
+                <Modal>
+                    <div>
+                        <div>
+                            <h2>Adicionar Funcionário</h2>
+                            <button>&times;</button>
+                        </div>
+                        <p>Adicione funcionários e ou usuários à sua unidade.</p>
+                    </div>
+                    <form action="" onSubmit={handleCreateUser}>
+
+                        {inputValues.map((item,index)=>{
+                            item.model === 'input' ? (
+                                <div className="fields">
+                                    <label>{item.label}</label>
+                                    <input name={item.name} type={item.type} />
+                                </div>
+                            ) : (
+                                <div className="fields">
+                                    <label>{item.label}</label>
+                                    <select name={item.name}>
+                                        <option>Outros</option>
+                                    </select>
+                                </div>
+                            )
+                        })}
+
+                        <button type="submit">Salvar</button>
+                    </form>
+                </Modal>
             </main>
         </>
     );
--- src/services/users.js
+++ src/services/users.js
@@ -14,3 +14,19 @@ export async function getUsers(){
 
     return data;
 }
+
+export async function createUsers(payload){
+    const response = await fetch(`${API_URL}/users`,{
+        method: 'POST',
+        headers:{'Type-Content':'application/json'},
+        body: JSON.stringify(payload)
+    });
+
+    const data = response.json();
+
+    if(!response.ok){
+        throw new Error(data.message || "Erro ao criar usuário.");
+    }
+
+    return data;
+}
